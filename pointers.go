aa450dd68dd1ff75ac0f2a7f3b0049a270646aea
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-22 12:09:15 -1000

    Pass a pointer to a function

--- pointers.go
+++ pointers.go
@@ -4,6 +4,17 @@ import (
 	"fmt"
 )
 
+// We are pointing to an address in memory where a type int is located
+// We are passing the value of an address
+func point(y *int) {
+	fmt.Println("y =", y)
+	fmt.Println("*y =", *y)
+	// Now we are derefering by taking the address and pointing to the value
+	*y = 55
+	fmt.Println("y =", y)
+	fmt.Println("*y =", *y)
+}
+
 func main() {
 	a := 42
 	fmt.Println("&a gives you the address of a in memory ", &a)
@@ -16,4 +27,11 @@ func main() {
 
 	*b = 123
 	fmt.Println("changing the value at the address of a means changing a's value ", a)
+
+	x := 0
+	fmt.Println("x =", x)
+	fmt.Println("&x =", &x)
+	point(&x)
+	fmt.Println("x =", x)
+	fmt.Println("&x =", &x)
 }
