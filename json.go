1fae79ca948b9b12b9813b048dbb3d64824e92cd
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-24 14:58:47 -1000

    Am I a Marshal ninja now?

--- json.go
+++ json.go
@@ -12,6 +12,12 @@ type ColorGroup struct {
 	Colors []string
 }
 
+type boat struct {
+	Make  string
+	Model string
+	Year  int
+}
+
 func main() {
 	group := ColorGroup{
 		ID:     1,
@@ -32,5 +38,29 @@ func main() {
 		fmt.Println("error:", error)
 	}
 
-	fmt.Printf("\n %+v is of Type %T", cg, cg)
+	fmt.Printf("\n %+v is of Type %T \n", cg, cg)
+
+	yat := boat{Make: "Amel", Model: "Maramu", Year: 2020}
+	cat := boat{Make: "Lagoon", Model: "50", Year: 2021}
+	boats := []boat{yat, cat}
+	fmt.Println("Boats", boats)
+
+	j, err := json.Marshal(boats)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Marshaled to json", string(j))
+
+	var yatchs []boat
+	err = json.Unmarshal([]byte(j), &yatchs)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("Unmarshaled from Json", yatchs)
+
+	for i, v := range yatchs {
+		fmt.Println("yatch number", i)
+		fmt.Println("\t", v.Make, v.Model, v.Year)
+	}
 }
