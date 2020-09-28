1bd7fd1dfffd6c83f07fe485d32f4abe2c15314d
Dmitry Sky <dmitry.sky@icloud.com>
2020-09-28 10:58:13 -1000

    Sort a slice of structs by their fields

--- sort.go
+++ sort.go
@@ -5,6 +5,11 @@ import (
 	"sort"
 )
 
+type person struct {
+	first string
+	age   int
+}
+
 func main() {
 	i := []int{5, 2, 6, 3, 1, 4}
 	sort.Ints(i)
@@ -13,4 +18,22 @@ func main() {
 	s := []string{"Go", "Bravo", "Gopher", "Alpha", "Grin", "Delta"}
 	sort.Strings(s)
 	fmt.Println(s)
+
+	p1 := person{"James", 32}
+	p2 := person{"Moneypenny", 27}
+	p3 := person{"Q", 64}
+	p4 := person{"M", 56}
+
+	people := []person{p1, p2, p3, p4}
+	fmt.Println(people)
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].first < people[j].first
+	})
+	fmt.Println("By First Name:", people)
+
+	sort.Slice(people, func(i, j int) bool {
+		return people[i].age < people[j].age
+	})
+	fmt.Println("By Age:", people)
 }
