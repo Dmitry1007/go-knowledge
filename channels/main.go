c9c0b2aeb8f7c042e35d9bec2352379389e9ebf7
Dmitry Sky <dmitry.sky@icloud.com>
2020-10-04 14:29:35 -1000

    Create a receive and a send directional channel

--- channels/main.go
+++ channels/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	// unbuffered channel
+	// Unbuffered bidirectional channel
 	c1 := make(chan int)
 	go func() {
 		c1 <- 42
@@ -11,10 +11,19 @@ func main() {
 	}()
 	fmt.Println(<-c1, <-c1)
 
-	// buffered channel
+	// Buffered bidirectional channel
 	// 1 is the buffer, meaning you can only place one value in the channel
 	c2 := make(chan int, 1)
 	c2 <- 57
 	fmt.Println(<-c2)
+	fmt.Printf("c2 is of Type %T \n", c2)
 
+	// Directional channels
+	// send only channel
+	c3 := make(chan<- int)
+	fmt.Printf("c3 is of Type %T \n", c3)
+
+	// receive only channel
+	c4 := make(<-chan int)
+	fmt.Printf("c4 is of Type %T \n", c4)
 }
