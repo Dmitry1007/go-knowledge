b180fd277d869d104074b52e0592fe05ab3eecd0
Dmitry Sky <dmitry.sky@icloud.com>
2020-10-08 14:33:29 -1000

    Example of actually canceling the context

--- channels/context/main.go
+++ channels/context/main.go
@@ -13,10 +13,19 @@ func main() {
 	fmt.Printf("context type:\t%T\n", ctx)
 	fmt.Println("------------")
 
-	ctx, _ = context.WithCancel(ctx)
+	ctx, cancel := context.WithCancel(ctx)
 
 	fmt.Println("context:\t", ctx)
 	fmt.Println("context err:\t", ctx.Err())
 	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("------------")
+
+	cancel()
+
+	fmt.Println("context:\t", ctx)
+	fmt.Println("context err:\t", ctx.Err())
+	fmt.Printf("context type:\t%T\n", ctx)
+	fmt.Println("cancel:\t\t", cancel)
+	fmt.Printf("cancel type:\t%T\n", cancel)
 	fmt.Println("------------")
 }
