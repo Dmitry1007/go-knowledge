cf450fba01d25ae90bf8abd5fc583a13feed23c7
Dmitry Sky <dmitry.sky@icloud.com>
2020-10-12 12:55:39 -1000

    Read from newly created file and output content

--- error-handling/os/main.go
+++ error-handling/os/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"strings"
 )
 
 func main() {
+	// Create file
 	f, err := os.Create("colors.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -16,6 +18,22 @@ func main() {
 	defer f.Close()
 
 	r := strings.NewReader("Red")
-
+	// Write to file
 	io.Copy(f, r)
+
+	// Open file
+	f, err = os.Open("colors.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer f.Close()
+	// Read from file
+	bs, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// Print read contents
+	fmt.Println(string(bs))
 }
