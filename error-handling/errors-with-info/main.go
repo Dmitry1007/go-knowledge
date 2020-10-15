e5c63dec599bdd43da2a327ebe0e945e37c08fe3
Dmitry Sky <dmitry.sky@icloud.com>
2020-10-15 12:08:23 -1000

    If a newly created struct has an Error method than it implements the Error interface and is of the type Error

--- error-handling/errors-with-info/main.go
+++ error-handling/errors-with-info/main.go
@@ -5,6 +5,16 @@ import (
 	"log"
 )
 
+type mathError struct {
+	lat  string
+	long string
+	err  error
+}
+
+func (m mathError) Error() string {
+	return fmt.Sprintf("a math error occured: %v %v %v", m.lat, m.long, m.err)
+}
+
 func main() {
 	_, err := sqrt(-10)
 	if err != nil {
@@ -15,7 +25,8 @@ func main() {
 func sqrt(f float64) (float64, error) {
 	if f < 0 {
 		// return 0, errors.New("Math: square root of negative number")
-		return 0, fmt.Errorf("Math: square root of negative number: %v", f)
+		me := fmt.Errorf("Math: square root of negative number: %v", f)
+		return 0, mathError{"25.5678 N", "74.3490 W", me}
 	}
 	return 42, nil
 }
